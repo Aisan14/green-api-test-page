7a2fb941dd8deb0d9cbbdd99dc5289bcab4c07ac
Aisan <119782359+Aisan14@users.noreply.github.com>
2024-06-21 12:12:04 +0500

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ async function fetchAPI(method, endpoint, data = {}) {
 
     const url = `${apiUrl}/waInstance${idInstance}/${endpoint}/${apiTokenInstance}`;
     try {
+        console.log(`Request URL: ${url}`);
+        console.log(`Request Data: ${JSON.stringify(data)}`);
+
         const response = await fetch(url, {
             method: method,
             headers: {
@@ -18,10 +21,17 @@ async function fetchAPI(method, endpoint, data = {}) {
             },
             body: method === 'POST' ? JSON.stringify(data) : null
         });
+
+        if (!response.ok) {
+            throw new Error(`Ошибка HTTP: ${response.status}`);
+        }
+
         const result = await response.json();
         document.getElementById('response').value = JSON.stringify(result, null, 2);
+        console.log(`Response Data: ${JSON.stringify(result)}`);
     } catch (error) {
         document.getElementById('response').value = 'Ошибка: ' + error.message;
+        console.error('Error:', error);
     }
 }
 
@@ -36,28 +46,34 @@ function getStateInstance() {
 function sendMessage() {
     const chatId = document.getElementById('chatId').value;
     const message = document.getElementById('message').value;
+
     if (!chatId || !message) {
         document.getElementById('response').value = 'Ошибка: Пожалуйста, введите chatId и message.';
         return;
     }
+
     const data = {
         chatId: chatId,
         message: message
     };
+
     fetchAPI('POST', 'sendMessage', data);
 }
 
 function sendFileByUrl() {
     const chatId = document.getElementById('fileChatId').value;
     const urlFile = document.getElementById('fileUrl').value;
+
     if (!chatId || !urlFile) {
         document.getElementById('response').value = 'Ошибка: Пожалуйста, введите chatId и urlFile.';
         return;
     }
+
     const data = {
         chatId: chatId,
         urlFile: urlFile,
         fileName: urlFile.split('/').pop()
     };
+
     fetchAPI('POST', 'sendFileByUrl', data);
 }
